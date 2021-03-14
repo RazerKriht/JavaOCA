02e6859275fbd6589c49b7ff352c89eab26b95a1
RazerKriht <p2H126ZuIZ>
2021-03-14 09:56:36 +0300

    Урок 6

---
+++ Lessons/src/Lesson6/Employee.java
@@ -0,0 +1,34 @@
+package Lesson6;
+
+public class Employee {
+    Employee(int id2, String surname2, int age2){
+        this(id2, surname2,age2,0.0,null);
+    }
+
+    Employee(String surname3, int age3){
+        this(0,surname3,age3,0.0,null);
+    }
+
+    Employee(int id4, String surname4, int age4, double salary4, String department4){
+        id = id4;
+        surname = surname4;
+        age = age4;
+        salary = salary4;
+        department = department4;
+    }
+
+    int id;
+    String surname;
+    int age;
+    double salary;
+    String department;
+}
+
+class  EmployeeTest{
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(1,"Vlad",20);
+        Employee emp2 = new Employee("Petrov",25);
+        Employee emp3 = new Employee(2,"Sidorov",40,100.35,"IT");
+
+    }
+}
---
+++ Lessons/src/Lesson6/MetodOverloading2.java
@@ -0,0 +1,27 @@
+package Lesson6;
+
+public class MetodOverloading2 {
+    public int sum(int i1, int i2){
+        return  i1+i2;
+    }
+
+    protected String sum(String s1, String s2){
+        return s1+s2;
+    }
+    protected void sum(int a, String b){
+        System.out.println("PRIVET!!");
+    }
+
+}
+
+class  MetodOverloading2Test{
+    public static void main(String[] args) {
+        MetodOverloading2 mO2 = new MetodOverloading2();
+
+        int a = mO2.sum(5,7);
+        System.out.println(a);
+        String s = mO2.sum("Hello, ", "World!");
+        System.out.println(s);
+
+    }
+}
