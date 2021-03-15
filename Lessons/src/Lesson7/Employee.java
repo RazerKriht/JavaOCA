657da832e383e17c748bde7996da0f11aa6d3bdc
RazerKriht <axewcol@gmail.com>
2021-03-15 11:31:10 +0300

    Урок 7

---
+++ Lessons/src/Lesson7/Employee.java
@@ -0,0 +1,14 @@
+package Lesson7;
+
+public class Employee {
+    double salary;
+
+    void dvoynayaZP(){
+        System.out.println("Novaya z/p= "+salary);
+
+    }
+
+    Employee(double salary2){
+        salary = salary2;
+    }
+}
