8853578b743a56f69e26cfb96c980329e2f7fd72
RazerKriht <axewcol@gmail.com>
2021-03-15 11:12:55 +0300

    Урок 7

--- Lessons/src/Lesson7/A.java
+++ Lessons/src/Lesson7/A.java
@@ -1,8 +1,11 @@
 package Lesson7;
 
+
 public class A {
     public static void main(String[] args) {
-        System.out.println("Privet vsem!");
-        ////
+        B objectB = new B();
+        Lesson6.Employee objEmployee = new Lesson6.Employee("Ivanov", 21);
+
+
     }
 }
--- Lessons/src/Lesson7/С.java
+++
@@ -1,7 +0,0 @@
-package Lesson7;
-
-public class С {
-    public static void main(String[] args) {
-
-    }
-}
