f6494df11f7c4dc51bffc2290558cdf126f25b3c
RazerKriht <axewcol@gmail.com>
2021-03-16 11:45:59 +0300

    Lesson 8

--- Lessons/src/Lesson8/Student.java
+++ Lessons/src/Lesson8/Student.java
@@ -4,17 +4,21 @@ public class Student {
     String name;
     int course;
     static int count;
+    int a;
 
     public Student(String name2, int course2){
         count++;
         name=name2;
         course=course2;
         System.out.println("Студент №"+count+" создан.");
-    }
-}
 
-class StudentTest{
-    public static void main(String[] args) {
+    }
+
+    public static void showCount(){
+        System.out.println("Count = "+count);
+    }
+
+    /* public static void main(String[] args) {
         Student st1 = new Student("Ivan",1);
         Student st2 = new Student("Peter",4);
         Student st3 = new Student("Masha",2);
@@ -22,5 +26,29 @@ class StudentTest{
         //Не хорошо!
         System.out.println(st1.count);
         System.out.println(st2.count);
+        //
+        Student.showCount();
+        st2.showCount();
+    }*/
+
+    public void showInfo(){
+        System.out.println("Welcome to the Student class!!");
+    }
+    void  abc(){
+        a++;
+        count++;
+    }
+
+    static void abcd(){
+        count++;
+        //a++; Нельзя!!!!!!
+        Student st2 = new Student("Petr",1);
+        st2.a++;
+    }
+
+    public static void main(String[] args) {
+        abcd();
+        //abc(); Нельзя!
+        Student st3 = new Student("Vlad",4);
     }
 }
--- Lessons/src/Lesson8/Test1.java
+++ Lessons/src/Lesson8/Test1.java
@@ -2,22 +2,26 @@ package Lesson8;
 
 public class Test1 {
 
-    public final int a;
+    //public final int a;
 
-    Test1(){
+    public static final int a=10;
+
+
+    /*Test1(){
         a=10;
     }
 
     Test1(boolean  b){
         a=10;
-    }
+    }*/
 
     public void abc(final short s){
         final byte b = 10;
     }
 
     public static void main(String[] args) {
-        Test1 t = new Test1();
-        System.out.println(t.a);
+        /*Test1 t = new Test1();
+        System.out.println(t.a);*/
+        System.out.println(Test1.a);
     }
 }
