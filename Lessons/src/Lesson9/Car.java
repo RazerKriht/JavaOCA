7184085883a4f45d6f580d7ac3fd51dbbd8f71ac
razerkrht <jUBorf6LtvPuFVEHgcuE>
2021-03-16 16:41:16 +0300

    Lesson 9

--- Lessons/src/Lesson9/Car.java
+++ Lessons/src/Lesson9/Car.java
@@ -2,14 +2,24 @@ package Lesson9;
 
 public class Car {
     String color = "blue";
-    String enjine = "V6";
-}
+    String engine = "V6";
+    static int count;
 
-class Human{
-    String name = "Ivan";
-    final Car c = new Car();
-    public static void main(String[] args) {
-        Human h1 = new Human();
-        h1.c.enjine="V12";
+    Car(String color, String engine){
+        count++;
+        this.color=color;
+        this.engine = engine;
     }
+
+    public void showColor(){
+        System.out.println("Цвет машины: "+color);
+    }
+
+    public void changeColor(String color){
+        System.out.println("Цвет машины изменился");
+        int cena = 5000;
+        this.color = color;
+        cena+=1000;
+    }
+
 }
