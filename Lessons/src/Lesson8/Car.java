1fde7e81dfabb7eaf5d161da6d3ecb514cb7c7fb
RazerKriht <axewcol@gmail.com>
2021-03-16 11:27:03 +0300

    Lesson 8

---
+++ Lessons/src/Lesson8/Car.java
@@ -0,0 +1,15 @@
+package Lesson8;
+
+public class Car {
+    String color = "blue";
+    String enjine = "V6";
+}
+
+class Human{
+    String name = "Ivan";
+    final Car c = new Car();
+    public static void main(String[] args) {
+        Human h1 = new Human();
+        h1.c.enjine="V12";
+    }
+}
