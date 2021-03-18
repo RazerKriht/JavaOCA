a89fae6f6f5b4f1f09e21e1db0c0c89ec21b23e8
RazerKriht <axewcol@gmail.com>
2021-03-18 11:43:42 +0300

    Lesson 12

---
+++ Lessons/src/Lesson12/Test13.java
@@ -0,0 +1,11 @@
+package Lesson12;
+
+public class Test13 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b=20;
+        int maximum = (a>b)?a:b;
+        System.out.println(maximum);
+    }
+
+}
